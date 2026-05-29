cd6c9d3bfd88c329ec89612135814620c432ba5b
ANAND SINGH <anisharawat324@gmail.com>
2026-05-29 21:47:31 +0530

    updated files

---
+++ phase-6.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class teacher{                             //base class -> parent class
+public:
+       string name;
+       string dept;
+
+       teacher(){
+          cout<<"Parent class constructor called"<<endl;
+       }
+
+       ~teacher(){
+          cout<<"Parent class destructor called"<<endl;
+       }
+};
+
+// parent class is derived in "public" mode
+class student:public teacher{              //derived class -> child class
+public:
+        string subject;
+
+        student(){
+            cout<<"student class constructor called"<<endl;
+        }       
+
+        ~student(){
+            cout<<"student class destructor called"<<endl;
+        }
+        void get_info(){
+            cout<<name<<endl<<dept<<endl<<subject<<endl;
+        }
+};
+
+int main(){
+    student s1;
+    s1.name="Anisha";
+    s1.dept="csai";
+    s1.subject="oops";
+
+    s1.get_info();
+    return 0;
+}
